413b45c6c469e1f0dce3a6bb68f046a9fbad4a78
Victor Kneider <victorkneider@gmail.com>
2023-11-17 17:33:02 -0400

    Update default profile picture URL

--- src/models/user.ts
+++ src/models/user.ts
@@ -71,7 +71,7 @@ const userSchema = new Schema<IUser>({
   },
   profilePicture: {
     type: String,
-    default: "",
+    default: "https://static.vecteezy.com/system/resources/thumbnails/008/442/086/small/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg",
   },
   firstLogin: {
     type: Boolean,
